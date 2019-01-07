c1edb40c820d37f7a0d12d46999b61ecd4ad4ea6
Grant Rodgers <grantrodgers@google.com>
2019-01-07 12:25:05 -0800

    Resolve SinkURI from the source's Sink reference
    For demonstration purposes and to reduce size and complexity of
    dependencies, an identical addressableType that duplicates AddressableType
    is defined in the controller package.
    
    Also updated test to verify expected SinkURI is present. The test suite
    creates a generic TestSink CRD to act as an Addressable.

--- pkg/controller/samplesource/samplesource_controller.go
+++ pkg/controller/samplesource/samplesource_controller.go
@@ -18,10 +18,14 @@ package samplesource
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	sourcesv1alpha1 "github.com/knative/sample-source/pkg/apis/sources/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -117,6 +121,64 @@ func (r *ReconcileSampleSource) Reconcile(request reconcile.Request) (reconcile.
 }
 
 func (r *ReconcileSampleSource) reconcile(ctx context.Context, instance *sourcesv1alpha1.SampleSource) error {
-	//TODO(user): Implement reconciliation
+	// Resolve the Sink URI based on the sink reference.
+	sinkURI, err := r.resolveSinkRef(ctx, instance.Spec.Sink)
+	if err != nil {
+		return fmt.Errorf("Failed to get sink URI: %v", err)
+	}
+
+	// Set the SinkURI field on the SampleSource Status.
+	instance.Status.SinkURI = sinkURI
+
+	//TODO(user): Add additional behavior.
 	return nil
+}
+
+// TODO(user): This is here to improve clarity and reduce the number of vendored
+// libraries. Consider using AddressableType from github.com/knative/pkg instead.
+type addressableType struct {
+	Status struct {
+		Address *struct {
+			Hostname string
+		}
+	}
+}
+
+// TODO(user): A version of this function is also available in the
+// github.com/knative/eventing-sources/pkg/controller/sinks package.
+func (r *ReconcileSampleSource) resolveSinkRef(ctx context.Context, sinkRef *corev1.ObjectReference) (string, error) {
+	// Make sure the reference is not nil.
+	if sinkRef == nil {
+		return "", fmt.Errorf("sink reference is nil")
+	}
+
+	//TODO(user): Add support for corev1.Service.
+
+	// Get the referenced Sink as an Unstructured object.
+	sink := &unstructured.Unstructured{}
+	sink.SetGroupVersionKind(sinkRef.GroupVersionKind())
+	if err := r.Get(ctx, client.ObjectKey{Namespace: sinkRef.Namespace, Name: sinkRef.Name}, sink); err != nil {
+		return "", fmt.Errorf("Failed to get sink object: %v", err)
+	}
+
+	// Marshal the Sink into an Addressable struct to more easily extract its
+	// hostname.
+	addressable := &addressableType{}
+	raw, err := sink.MarshalJSON()
+	if err != nil {
+		return "", fmt.Errorf("Failed to marshal sink: %v", err)
+	}
+	if err := json.Unmarshal(raw, addressable); err != nil {
+		return "", fmt.Errorf("Failed to marshal sink into Addressable: %v", err)
+	}
+
+	// Check that the Addressable fields are present.
+	if addressable.Status.Address == nil {
+		return "", fmt.Errorf("Failed to resolve sink URI: sink does not contain address")
+	}
+	if addressable.Status.Address.Hostname == "" {
+		return "", fmt.Errorf("Failed to resolve sink URI: address hostname is empty")
+	}
+	// Translate the Hostname into a URI.
+	return fmt.Sprintf("http://%s/", addressable.Status.Address.Hostname), nil
 }
